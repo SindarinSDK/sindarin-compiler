296840216c7c8571eb536a9c5c9e2df33a68cc33
RealOrko <RealOrko@nowhere>
2026-01-22 20:36:44 +0000

    Fix windows ...

--- src/gcc_backend.c
+++ src/gcc_backend.c
@@ -101,9 +101,7 @@ static const char *translate_lib_name(const char *lib)
         const char *from;
         const char *to;
     } win_mappings[] = {
-        { "z", "zlib" },       /* zlib uses "libzlib.a" on Windows, "libz.a" on Unix */
-        { "xml2", "libxml2" }, /* libxml2 uses "libxml2" on Windows */
-        { "yaml", "libyaml" }, /* libyaml uses "libyaml" on Windows */
+        { "z", "zlib" },  /* zlib uses "libzlib.a" on Windows, "libz.a" on Unix */
         { NULL, NULL }
     };
 
