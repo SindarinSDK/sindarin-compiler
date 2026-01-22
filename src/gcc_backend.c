e2b869ae183f651808ad94e91f8b4962a1596edf
RealOrko <realorko@nowhere>
2026-01-22 20:04:40 +0000

    Implemented libxml in the SDK

--- src/gcc_backend.c
+++ src/gcc_backend.c
@@ -107,7 +107,8 @@ static const char *translate_lib_name(const char *lib)
         const char *from;
         const char *to;
     } win_mappings[] = {
-        { "z", "zlib" },  /* zlib uses "zlib" on Windows, "z" on Unix */
+        { "z", "zlib" },     /* zlib uses "zlib" on Windows, "z" on Unix */
+        { "xml2", "libxml2" }, /* libxml2 uses "libxml2" on Windows */
         { NULL, NULL }
     };
 
