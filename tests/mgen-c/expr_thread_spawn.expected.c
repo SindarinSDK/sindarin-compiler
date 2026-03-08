cc3b06884e0a4edc560d24f60967a56d3aa3f414
realorko <realorko@nowhere.com>
2026-03-08 01:25:56 +0000

    Add 9 new mgen/mgen-c test pairs for codegen coverage
    Tests: sized_array, static_method, match_multi, call_method,
    typeof, is, as_type, fn_native, thread_spawn (53 total per suite)

---
+++ tests/mgen-c/expr_thread_spawn.expected.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <limits.h>
+#include <setjmp.h>
+#include "runtime.h"
+#ifdef _WIN32
+#undef min
+#undef max
+#endif
+
+
+/* Closure type for lambdas */
+typedef struct __Closure__ { void *fn; RtArenaV2 *arena; size_t size; } __Closure__;
+
+
+/* Forward declarations */
+static RtArenaV2 *__main_arena__ = NULL;
+
+long long __sn__compute(RtArenaV2 *);
+
+long long __sn__compute(RtArenaV2 *__caller_arena__) {
+    rt_safepoint_poll();
+    RtArenaV2 *__local_arena__ = __caller_arena__;
+    long long _return_value = 0;
+
+    _return_value = 42LL; goto __sn__compute_return;
+__sn__compute_return:
+    return _return_value;
+}
+
+int main() {
+    rt_safepoint_init();
+    rt_safepoint_thread_register();
+    RtArenaV2 *__local_arena__ = rt_arena_v2_create(NULL, RT_ARENA_MODE_DEFAULT, "main");
+    __main_arena__ = __local_arena__;
+    int _return_value = 0;
+    long long __sn__handle = ;
+    long long __sn__result = ;
+    _return_value = __sn__result; goto main_return;
+main_return:
+    rt_arena_v2_condemn(__local_arena__);
+    return _return_value;
+}
